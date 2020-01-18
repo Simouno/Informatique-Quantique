0a62a9d9a615164d597e2554f7a0ea2f2f64cbf3
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-18 05:57:32 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,26 @@
-
+from simplecrypt import encrypt, decrypt
+from pycrypto import *
+from time import *
+# Funtions
+def ask()
+	print("1 pour encripter, 2 pour decripter")
+	x=int(input("Choix>"))
+	if x == 1:
+		encript()
+	elif x == 2:
+		decript()
+	else:
+		print("reponse inconue, sois 1 ou 2")
+def encript():
+	Text=str(input("Message> "))
+	Clef=str(input("Clef> "))
+	ciphertext = encrypt(Clef, Text)
+	print(ciphertext)
+def decript()
+	Text=str(input("Message> "))
+	Clef=str(input("Clef> "))
+	plaintext = decrypt(Clef, Text)
+# Start
+While 1:
+	ask()
+	
